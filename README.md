f6af944579356b766dc41151c142420ba4f904c2
Aradhya Pavan H S <aradhyapavan@aol.com>
2023-12-26 18:38:21 +0530

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# curated-movies
--- static/js/sjs.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 5,
-    spaceBetween: 10,
-    slidesPerGroup: 4,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
